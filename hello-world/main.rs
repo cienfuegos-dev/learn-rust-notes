eae5d910512984d6f573c32b2a944600e2cd528e
okcodes <ok@ok.codes>
2023-12-05 04:02:05 -0600

    Clean main

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,7 +1,2 @@
-use say_hello_useless;
-
 fn main() {
-    say_hello_useless::pub_say_hello_outter();
-    say_hello_useless::pub_hi::pub_say_hi_inner();
-    println!("Hello")
 }
---
+++ hello-world/using_my_crate.rs.example
@@ -0,0 +1,7 @@
+use say_hello_useless;
+
+fn main() {
+    say_hello_useless::pub_say_hello_outter();
+    say_hello_useless::pub_hi::pub_say_hi_inner();
+    println!("Hello")
+}
